8433a09140e4f5f83cf64e73ec95e0aae0084064
Dwight Spencer <dspencer@computekindustries.com>
2020-08-16 23:31:43 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+// Should create new assume role for turnkey linux hub
+resource "aws_iam_policy" "turnkeyhub_policy" {
+  name        = "turnkeyhub-policy"
+  path        = "/"
+  description = "Turnkey hub IAM policy"
+  policy      = data.aws_iam_policy_document.turnkeyhub_grants.json
+  tags = {
+    owner = "denzuko"
+  }
+}
+
+resource "aws_iam_role" "turnkeyhub_role" {
+  name = "turnkeyhub"
+
+  assume_role_policy = data.aws_iam_policy_document.turnkeyhub_assume_role.json
+  
+  tags = {
+    owner = "denzuko"
+  }
+}
